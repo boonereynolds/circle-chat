f124b670c0e2ccc73430298513663e7a269ec4cc
Boone Reynolds <boone.reynolds@gmail.com>
2017-04-10 12:31:05 -0700

    initializes controllers and create users contoller

---
+++ api/controllers/users_controller.js
@@ -0,0 +1,56 @@
+var User = require('../models.user')
+
+// creates User
+function createUser(req, res){
+  var user = new User(req.body)
+
+  user.save(function(err, user){
+    if (err) res.status(500).send(err)
+    res.status(201).send(user)
+  })
+}
+
+// shows a single User
+function showUser(req, res){
+  User.find({_id: req.params.id}, function(err, user){
+    if (err) res.status(404).send(err)
+    res.status(200).send(user)
+  })
+}
+
+// update a User
+function updateUser(req, res){
+  User.find({_id: req.params.id}, function(err, user){
+    if (err) res.status(404).send(err)
+
+    if(req.body.username) bean.username = req.body.username
+    if(req.body.email) bean.email = req.body.email
+    if(req.body.password) bean.password = req.body.password
+    if(req.body.location) bean.location = req.body.location
+    if(req.body.chats) bean.chats = req.body.chats
+
+    user.save(function(err){
+      if (err) res.status(500).send(err)
+      res.status(200).send(user)
+    })
+  })
+}
+
+// delete a User
+function deleteUser(req, res){
+  User.remove({_id: req.params.id}, function(err){
+    if (err) res.status(500).send(err)
+    res.status(200).send({message: "User successfully deleted!"})
+  })
+}
+
+
+
+
+// exports module
+module.exports = {
+  createUser: createUser,
+  showUser: showUser,
+  updateUser: updateUser,
+  deleteUser: deleteUser
+}
