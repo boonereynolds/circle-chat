d2ef2ccfb73e3713f4230f37c7a02ea744f87da7
Boone Reynolds <boone.reynolds@gmail.com>
2017-04-13 08:57:37 -0700

    backend is fully function but there is no authentication

--- api/controllers/chats_controller.js
+++ api/controllers/chats_controller.js
@@ -1,4 +1,14 @@
 var Chat = require('../models/chat')
+// res.json instead of res.status
+
+
+// index function to show all chats
+function chatIndex(req, res){
+  Chat.find({}, function(err, chats){
+    if (err) res.status(404).send(err)
+    res.status(200).send(chats)
+  })
+}
 
 // create chat
 function createChat(req, res){
@@ -10,7 +20,17 @@ function createChat(req, res){
   })
 }
 
+// show function to show a single chat with the messages
+function showChat(req, res){
+  Chat.find({_id: req.params.id}, function(err, chat){
+    if (err) res.status(404).send(err)
+    res.status(200).send(chat)
+  })
+}
+
 // exports module
 module.exports = {
-  createChat: createChat
+  chatIndex: chatIndex,
+  createChat: createChat,
+  showChat: showChat
 }
--- api/models/user.js
+++ api/models/user.js
@@ -9,6 +9,21 @@ var userSchema = new mongoose.Schema({
   chats: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 })
 
+// add bcrypt hashing to model (works on a password field)!
+userSchema.plugin(require('mongoose-bcrypt'))
+
+// Add a "transformation" to the model's toJson function that
+// stops the password field (even in digest format) from being
+// returned in any response.
+// transformation does not return user's password,
+// this function (anytime .toJSON is called) deletes password from returned object
+userSchema.options.toJSON = {
+  transform: function(document, returnedObject, options) {
+    delete returnedObject.password
+    return returnedObject
+  }
+}
+
 // sets variable for model
 var User = mongoose.model('User', userSchema)
 
